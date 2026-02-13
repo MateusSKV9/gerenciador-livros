76cdc78121523c41af3fdc2517677e0734d37177
Mateus Santos <mateussilvanzg@gmail.com>
2026-02-13 20:37:03 -0300

    feat: add copyright information to footer and description to index

--- src/shared/layout/Footer/Footer.tsx
+++ src/shared/layout/Footer/Footer.tsx
@@ -4,8 +4,17 @@ export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<p>
-				Site desenvolvido por <a href="#">Mateus Santos</a>
+				Site desenvolvido por{" "}
+				<a
+					className={styles.link}
+					href="https://www.linkedin.com/in/mateus-santos-1a7361246/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Mateus Santos
+				</a>
 			</p>
+			<span className={styles.rights}>© {new Date().getFullYear()} Mateus Santos</span>
 		</footer>
 	);
 };
